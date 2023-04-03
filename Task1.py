1fffdd0573fa02574cceb7a64403ec70eb377392
Mrunmayee Jagtap <124246283+mrunmayeejagtap@users.noreply.github.com>
2023-04-04 00:28:56 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,8 +1,16 @@
+
 # Import statements
 import numpy as np
 import argparse
 import cv2
 import os
+
+"""
+Download the model files: 
+	1. colorization_deploy_v2.prototxt:    https://github.com/richzhang/colorization/tree/caffe/colorization/models
+	2. pts_in_hull.npy:	https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+	3. colorization_release_v2.caffemodel:    https://www.dropbox.com/s/dx0qvhhp5hbcx7z/colorization_release_v2.caffemodel?dl=1
+"""
 
 
 # Paths to load the model
